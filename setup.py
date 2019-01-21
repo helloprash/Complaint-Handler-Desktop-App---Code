fcee7f23b125c21254979679ab995102e96a2174
Prashanth Kumar <31364644+helloprash@users.noreply.github.com>
2019-01-21 23:21:47 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+import cx_Freeze
+import sys
+import os
+import selenium
+
+base = None
+
+os.environ['TCL_LIBRARY'] = r'D:\Users\PB6\AppData\Local\Programs\Python\Python37\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'D:\Users\PB6\AppData\Local\Programs\Python\Python37\tcl\tk8.6'
+
+
+syspath = r"D:\Users\PB6\AppData\Local\Programs\Python\Python37\DLLs"
+
+includes      = []
+include_files = [syspath + '/tcl86t.dll', syspath + '/tk86t.dll', 'jnjGIF.gif', 'chromedriver.exe', 'phantomjs.exe']
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+executables = [cx_Freeze.Executable("UI.py", base=base, icon="Jnj32.ico")]
+
+cx_Freeze.setup(
+    name = "CatsWeb Complaint Handler",
+    options = {"build_exe": {"includes": includes, "include_files": include_files,"packages":["tkinter","selenium"]}},
+    version = "1.0.0",
+    description = "CatsWeb complaint handler for Level 1 and Level 2 complaints",
+    executables = executables
+    )
