8621af26905fca7cf7a910d5180a8b9458ff2551
Prashanth Kumar <31364644+helloprash@users.noreply.github.com>
2019-01-22 16:50:48 +0530

    Fixed Timeout Error

--- complaint_handler.py
+++ complaint_handler.py
@@ -42,6 +42,7 @@ def actionSubmit(browser,ID):
 def getCFDetails(htmlSource):
     try:
         soup = BS(htmlSource, "lxml")
+        #soup = BS(open(file_path), "lxml")
         center = soup.find_all('center')
         tables = soup.find_all('table',{'id':'TBCALogForm'})
 
@@ -51,6 +52,13 @@ def getCFDetails(htmlSource):
             if eachtd:
                 data = [each.find('br').next_sibling for each in eachtd]
         username = data[0].text.strip()
+
+        #Medical event
+        td = [tr.find_all('td', {'id':'TDStandardText069'}) for tr in tables[0].find_all('tr')]
+        for eachtd in td:
+            if eachtd:
+                data = [each.find('font') for each in eachtd]
+        medical_event = data[0].text.strip()
 
         #RDPC
         td = [tr.find_all('td', {'id':'TDStandardMemo013'}) for tr in tables[0].find_all('tr')]
@@ -105,9 +113,10 @@ def getCFDetails(htmlSource):
             IRstep = 'XX'
             IRnum = 'XXXX'
         
-        return(True,username,RDPC,step,productType,productFormula,serialNum,IR,IRstep,IRnum)
-    except:
-        return(False,'username','RDPC','step','productType','productFormula','serialNum','IR','IRstep','IRnum')
+        return(True,username,RDPC,medical_event,step,productType,productFormula,serialNum,IR,IRstep,IRnum)
+    except Exception as err:
+        print(err)
+        return(False,'username','RDPC','medical_event','step','productType','productFormula','serialNum','IR','IRstep','IRnum')
 
 
 def Login(ID, password):
@@ -116,38 +125,36 @@ def Login(ID, password):
     url = 'http://cwqa/CATSWebNET/'
     #url = 'http://cwqa/CATSWebNET/main.aspx?WCI=Main&WCE=ViewDashboard&WCU=s%3dSA8IA9EM9TG18C4I98HEXMXU46CTV2XO%7c*~r%3dComplaint%20Owner%20Home%20Page%7c*~q%3d1'
     
-    try :
-        browser = webdriver.PhantomJS(executable_path = pjs_file, desired_capabilities={'phantomjs.page.settings.resourceTimeout': '5000'})
-        browser.implicitly_wait(3)
-        browser.set_page_load_timeout(100) 
-        browser.get(url)
+    while True:
+        try :
+            browser = webdriver.PhantomJS(executable_path = pjs_file, desired_capabilities={'phantomjs.page.settings.resourceTimeout': '5000'})
+            browser.implicitly_wait(3)
+            browser.set_page_load_timeout(100) 
+            browser.get(url)
 
-        browser.save_screenshot('firstCheck.png')
+            browser.save_screenshot('firstCheck.png')
 
-        browser.find_element_by_xpath('//*[@id="CTRLemployeeid"]').send_keys(ID)
-        browser.find_element_by_xpath('//*[@id="CTRLPasswordPrompt"]').send_keys(password)
-        browser.find_element_by_xpath('//*[@id="TBLoginForm"]/tbody/tr[6]/td[2]/button').click()
+            browser.find_element_by_xpath('//*[@id="CTRLemployeeid"]').send_keys(ID)
+            browser.find_element_by_xpath('//*[@id="CTRLPasswordPrompt"]').send_keys(password)
+            browser.find_element_by_xpath('//*[@id="TBLoginForm"]/tbody/tr[6]/td[2]/button').click()
 
-        sessionFlag, returnMsg = checkSession(browser.page_source)
-        if not sessionFlag:
-            return (returnMsg, browser.current_url, sessionFlag)
+            sessionFlag, returnMsg = checkSession(browser.page_source)
+            if not sessionFlag:
+                return (returnMsg, browser.current_url, sessionFlag)
 
-        browser.save_screenshot('secondCheck.png')
+            browser.save_screenshot('secondCheck.png')
 
-        return ('Please enter your login information:',browser.current_url, True)
-
-
-        '''
-        if browser.current_url == 'http://cwqa/CATSWebNET/main.aspx?WCI=Main&WCE=Login':
-            return ('Invalid login, please try again.',None, False)
-        else:
             return ('Please enter your login information:',browser.current_url, True)
-        '''
-          
-    except (urllib3.exceptions.TimeoutError, urllib3.exceptions.ReadTimeoutError): 
-        print("Page load Timeout Occured. Quiting !!!")
-        browser.quit()
-        return ('Page load Timeout Occured. Please try again',None, False)
+              
+        except (urllib3.exceptions.TimeoutError, urllib3.exceptions.ReadTimeoutError): 
+            print("Page load Timeout Occured. Quiting !!!")
+            browser.quit()
+            continue
+            #return ('Page load Timeout Occured. Please try again',None, False)
+
+        except:
+            browser.quit()
+            continue
     
     #return (url, True)
 
@@ -159,29 +166,37 @@ def preview(CFnum, main_url):
     #chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("disable-extensions")
-    try:
-        browser = webdriver.Chrome(pjs_file, chrome_options=chrome_options)
-        browser.implicitly_wait(3)
-        browser.set_page_load_timeout(100)
-        browser.get(main_url)
+    while True:
+        try:
+            browser = webdriver.Chrome(pjs_file, chrome_options=chrome_options)
+            browser.implicitly_wait(3)
+            browser.set_page_load_timeout(100)
+            browser.get(main_url)
 
-        sessionFlag, returnMsg = checkSession(browser.page_source)
-        if not sessionFlag:
-            if returnMsg == 'Your CATSWeb V7 session does not exist.  Please enter your login information:':
-                return sessionFlag, False, 'Your CATSWeb V7 session expired!'
-       
-        print(browser.current_url)
+            sessionFlag, returnMsg = checkSession(browser.page_source)
+            if not sessionFlag:
+                if returnMsg == 'Your CATSWeb V7 session does not exist.  Please enter your login information:':
+                    return sessionFlag, False, 'Your CATSWeb V7 session expired!'
+           
+            print(browser.current_url)
 
-        CM = checkCM(browser.page_source)
-        if CM:
-            browser.find_element_by_xpath('//*[@id="TDDisplayPart004"]/font/a/font').click() #Load Complaint Owner Home page
+            CM = checkCM(browser.page_source)
+            if CM:
+                browser.find_element_by_xpath('//*[@id="TDDisplayPart004"]/font/a/font').click() #Load Complaint Owner Home page
 
-        browser = actionSubmit(browser,CFnum)
-        return sessionFlag,True, 'None'
+            browser = actionSubmit(browser,CFnum)
+            return sessionFlag,True, 'None'
 
-    except (urllib3.exceptions.TimeoutError, urllib3.exceptions.ReadTimeoutError):
-        print('Read Timeout Error')
-        return sessionFlag, False, 'Read Timeout Error!'
+        except (urllib3.exceptions.TimeoutError, urllib3.exceptions.ReadTimeoutError):
+            print('Read Timeout Error')
+            browser.quit()
+            continue
+            #return sessionFlag, False, 'Read Timeout Error!'
+
+        except:
+            browser.quit()
+            continue
+
 
 def complaintProcess(CFnum, url):
     print('inside complaintProcess', url)
@@ -195,50 +210,63 @@ def complaintProcess(CFnum, url):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("disable-extensions")
     '''
-    try:
-        #browser = webdriver.Chrome(pjs_file, chrome_options=chrome_options)
-        
-        browser = webdriver.PhantomJS(executable_path = pjs_file, desired_capabilities={'phantomjs.page.settings.resourceTimeout': '5000'})
-        browser.implicitly_wait(3)
-        browser.set_page_load_timeout(100)
+    while True:
+        try:
+            #browser = webdriver.Chrome(pjs_file, chrome_options=chrome_options)
+            
+            browser = webdriver.PhantomJS(executable_path = pjs_file, desired_capabilities={'phantomjs.page.settings.resourceTimeout': '5000'})
+            browser.implicitly_wait(3)
+            browser.set_page_load_timeout(100)
 
-        browser.get(url)
+            browser.get(url)
 
-        sessionFlag, returnMsg = checkSession(browser.page_source)
-        if not sessionFlag:
-            if returnMsg == 'Your CATSWeb V7 session does not exist.  Please enter your login information:':
-                return False, CFnum, 'session expired!', False
-       
-        print(browser.current_url)
+            sessionFlag, returnMsg = checkSession(browser.page_source)
+            if not sessionFlag:
+                if returnMsg == 'Your CATSWeb V7 session does not exist.  Please enter your login information:':
+                    return False, CFnum, 'session expired!', False
+           
+            print(browser.current_url)
 
-        CM = checkCM(browser.page_source)
-        if CM:
-            browser.find_element_by_xpath('//*[@id="TDDisplayPart004"]/font/a/font').click()
+            CM = checkCM(browser.page_source)
+            if CM:
+                browser.find_element_by_xpath('//*[@id="TDDisplayPart004"]/font/a/font').click()
 
-        browser = actionSubmit(browser,CFnum)
-        
-        (flag, username,RDPC,current_step,productType,productFormula,serialNum,IR,IRstep,IRnum) = getCFDetails(browser.page_source)
-        print(flag, username,RDPC,current_step,productType,productFormula,serialNum,IR,IRstep,IRnum)
+            browser = actionSubmit(browser,CFnum)
+            
+            (flag, username,RDPC,medical_event,current_step,productType,productFormula,serialNum,IR,IRstep,IRnum) = getCFDetails(browser.page_source)
+            print(flag, username,RDPC,medical_event,current_step,productType,productFormula,serialNum,IR,IRstep,IRnum)
+            break
 
-    except (urllib3.exceptions.TimeoutError, urllib3.exceptions.ReadTimeoutError):
-        print('Read Timeout Error')
-        return (True, CFnum, 'Read Timeout Error', False)
+        except (urllib3.exceptions.TimeoutError, urllib3.exceptions.ReadTimeoutError):
+            print('Read Timeout Error')
+            browser.quit()
+            continue
+            #return (True, CFnum, 'Read Timeout Error', False)
 
-    except NoSuchElementException as allError:
-        return (True, CFnum, allError, False)
+        except NoSuchElementException as allError:
+            browser.quit()
+            return (True, CFnum, allError, False)
+
+        except:
+            browser.quit()
+            continue
 
 
-    if not flag:
-        browser.quit()
-        return (True, CFnum,'This is not a valid complaint folder number', False)
-
-        
     process_steps = {
                         '050':steps.step90,
                         '090':steps.step140,
                         '140':steps.step999
                     }
-    if current_step == '999':
+
+    if not flag:
+        browser.quit()
+        return (True, CFnum,'This is not a valid complaint folder number', False)
+
+    elif medical_event == 'Yes':
+        browser.quit()
+        return (True, CFnum,'Medical event is Yes. Cannot process', False)
+
+    elif current_step == '999':
         print('IR still open in step {}'.format(IRstep))
         browser.quit()
         return (True, CFnum, 'Complaint folder already closed - step {}'.format(current_step), False)
